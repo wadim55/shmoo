f80c8002f0a9525d9fa5516bda53a4eaad21cd70
wadim55 <korkin_wadim@mail.ru>
2022-12-24 18:09:46 +0600

    Проект группы 5ч

--- Assets/Scripts/EnemyTakeDamage.cs
+++ Assets/Scripts/EnemyTakeDamage.cs
@@ -2,13 +2,24 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyTakeDamage : MonoBehaviour
 {
+  
+   [SerializeField] private GameObject BlueLelik;
+   [SerializeField] private GameObject hips;
+   [SerializeField] private GameObject boom;
    private void OnMouseDown()
    {
       gameObject.tag = "target";
       EVENT.ZvonokSobitie(1);
       gameObject.tag = "no";
+      BlueLelik.GetComponent<NavMeshAgent>().enabled = false;
+      BlueLelik.GetComponent<Robot>().enabled = false;
+      BlueLelik.GetComponent<Animator>().enabled = false;
+      hips.SetActive(true);
+      Destroy(BlueLelik,3);
+      Instantiate(boom, transform.position, Quaternion.identity);
    }
 }
